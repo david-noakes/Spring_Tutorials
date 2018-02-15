e5b423544c38c0a05c97fcd7c48c88b31cfbdfa2
david noakes <david.noakes@gmail.com>
2018-02-15 17:19:29 +1000

    Using appContext to create the classes

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,14 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		Fruit myFruit = new Fruit();
-		Vegetable myVegetable = new Vegetable();
+//		Fruit myFruit = new Fruit();
+//		Vegetable myVegetable = new Vegetable();
+
+		ApplicationContext appContext = new FileSystemXmlApplicationContext("appContext.xml");
+		
+		Fruit myFruit = appContext.getBean("fruit", Fruit.class);
+		Vegetable myVegetable = (Vegetable) appContext.getBean("vegetable");
 		
 		System.out.println(myFruit.talkABoutYourself());
 		System.out.println(myVegetable.talkAboutYourself());

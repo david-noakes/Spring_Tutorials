341310b7bb54d3466123d20ca72b3471b800edaa
david noakes <david.noakes@gmail.com>
2018-02-19 14:14:46 +1000

    Autowire tutorial - by constructor

--- AutowireDemo/src/main/java/com/example/demo/AutowireDemoApplication.java
+++ AutowireDemo/src/main/java/com/example/demo/AutowireDemoApplication.java
@@ -4,6 +4,7 @@ package com.example.demo;
  * v001 - initial version
  * v002 - autowire by type (beans must be unique by type, or set as primary OR autowire-candidate="true" | "false"
  * v003 - autowire by name vegetable does not match vegie, and default action is crash
+ * v004 - autowire by constructor
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,10 +26,13 @@ public class AutowireDemoApplication {
 		System.out.println("Spring meal: " + springMeal.whatsInThisMeal());
 		
 		Meal autowireMealByType = appContext.getBean("mealAutowiredByType", Meal.class);
-		System.out.println("Autowired meal: " + autowireMealByType.whatsInThisMeal());
+		System.out.println("Autowired Type meal: " + autowireMealByType.whatsInThisMeal());
 		
 		Meal autowireMealByName = appContext.getBean("mealAutowiredByName", Meal.class);
-		System.out.println("Autowired meal: " + autowireMealByName.whatsInThisMeal());
+		System.out.println("Autowired Name meal: " + autowireMealByName.whatsInThisMeal());
+
+		Meal mealAutowiredByConstructor = appContext.getBean("mealAutowiredByConstructor", Meal.class);
+		System.out.println("Autowired Constructor meal: " + mealAutowiredByConstructor.whatsInThisMeal());
 
 		((ClassPathXmlApplicationContext) appContext).close();
 		

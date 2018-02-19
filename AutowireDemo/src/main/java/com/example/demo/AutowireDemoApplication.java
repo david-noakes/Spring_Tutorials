c80295b73196fe57ae2cef6bcf82f50508cf3644
david noakes <david.noakes@gmail.com>
2018-02-19 14:02:36 +1000

    Autowire by name demo

--- AutowireDemo/src/main/java/com/example/demo/AutowireDemoApplication.java
+++ AutowireDemo/src/main/java/com/example/demo/AutowireDemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 /*
  * v001 - initial version
  * v002 - autowire by type (beans must be unique by type, or set as primary OR autowire-candidate="true" | "false"
+ * v003 - autowire by name vegetable does not match vegie, and default action is crash
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,9 +24,11 @@ public class AutowireDemoApplication {
 		Meal springMeal = appContext.getBean("meal", Meal.class);
 		System.out.println("Spring meal: " + springMeal.whatsInThisMeal());
 		
-		Meal autowireMeal = appContext.getBean("mealAutowired", Meal.class);
-		System.out.println("Autowired meal: " + autowireMeal.whatsInThisMeal());
+		Meal autowireMealByType = appContext.getBean("mealAutowiredByType", Meal.class);
+		System.out.println("Autowired meal: " + autowireMealByType.whatsInThisMeal());
 		
+		Meal autowireMealByName = appContext.getBean("mealAutowiredByName", Meal.class);
+		System.out.println("Autowired meal: " + autowireMealByName.whatsInThisMeal());
 
 		((ClassPathXmlApplicationContext) appContext).close();
 		
